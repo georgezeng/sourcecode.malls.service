f7137f7eb0bfbbd384df5b4bc69fd85d4ade7305
georgezeng <zzeng@bglcorp.com.au>
2019-06-16 14:23:53 +0800

    no message

---
+++ src/main/java/com/sourcecode/malls/web/security/filter/DruidStatFilter.java
@@ -0,0 +1,16 @@
+package com.sourcecode.malls.web.security.filter;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+
+import com.alibaba.druid.support.http.WebStatFilter;
+
+
+@WebFilter(
+        urlPatterns = "/druid/*",
+        initParams = {
+                @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*")
+        }
+)
+public class DruidStatFilter extends WebStatFilter {
+}
