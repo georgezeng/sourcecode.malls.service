3aa1328ffc6adb95c0064bf109d38638924b0298
georgezeng <zzeng@bglcorp.com.au>
2019-06-16 20:51:36 +0800

    no message

---
+++ src/main/java/com/sourcecode/malls/config/DruidConfiguration.java
@@ -0,0 +1,10 @@
+package com.sourcecode.malls.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.druid.spring.boot.autoconfigure.stat.DruidSpringAopConfiguration;
+
+@Configuration
+public class DruidConfiguration extends DruidSpringAopConfiguration {
+
+}
