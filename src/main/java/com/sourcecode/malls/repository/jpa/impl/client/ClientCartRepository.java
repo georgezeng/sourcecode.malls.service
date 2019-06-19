4e6506b08a7a5bc98cac903ab2ef5781c1547a93
georgezeng <zzeng@bglcorp.com.au>
2019-06-19 12:34:11 +0800

    no message

--- src/main/java/com/sourcecode/malls/repository/jpa/impl/client/ClientCartRepository.java
+++ src/main/java/com/sourcecode/malls/repository/jpa/impl/client/ClientCartRepository.java
@@ -8,10 +8,13 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.sourcecode.malls.domain.client.Client;
 import com.sourcecode.malls.domain.client.ClientCartItem;
+import com.sourcecode.malls.domain.goods.GoodsItem;
 import com.sourcecode.malls.domain.goods.GoodsItemProperty;
 
 public interface ClientCartRepository extends JpaRepository<ClientCartItem, Long>, JpaSpecificationExecutor<ClientCartItem> {
 	List<ClientCartItem> findByClient(Client client);
 
+	List<ClientCartItem> findByClientAndItem(Client client, GoodsItem item);
+	
 	Optional<ClientCartItem> findByClientAndProperty(Client client, GoodsItemProperty property);
 }
