dd0d5b15311d800d6215573520dcfee1fc0400ea
georgezeng <zzeng@bglcorp.com.au>
2019-06-27 14:05:55 +0800

    no message

--- src/main/java/com/sourcecode/malls/dto/goods/GoodsItemEvaluationDTO.java
+++ src/main/java/com/sourcecode/malls/dto/goods/GoodsItemEvaluationDTO.java
@@ -1,9 +1,18 @@
 package com.sourcecode.malls.dto.goods;
 
 public class GoodsItemEvaluationDTO {
+	private Long id;
 	private String clientNickname;
 	private String remark;
 	private String clientAvatar;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getClientNickname() {
 		return clientNickname;
