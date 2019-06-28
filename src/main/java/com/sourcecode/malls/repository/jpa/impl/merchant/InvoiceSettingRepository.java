cff2fbd9512ec63890e3ad1f93920d9d9f6ae9fe
georgezeng <zzeng@bglcorp.com.au>
2019-06-28 22:34:08 +0800

    no message

--- src/main/java/com/sourcecode/malls/repository/jpa/impl/merchant/InvoiceSettingRepository.java
+++ src/main/java/com/sourcecode/malls/repository/jpa/impl/merchant/InvoiceSettingRepository.java
@@ -1,5 +1,7 @@
 package com.sourcecode.malls.repository.jpa.impl.merchant;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +13,8 @@ import com.sourcecode.malls.domain.merchant.Merchant;
 public interface InvoiceSettingRepository
 		extends JpaRepository<InvoiceSetting, Long>, JpaSpecificationExecutor<InvoiceSetting> {
 	Page<InvoiceSetting> findAllByMerchant(Merchant merchant, Pageable pageable);
+
+	List<InvoiceSetting> findAllByMerchantOrderByOrderNumAsc(Merchant merchant);
 
 	Page<InvoiceSetting> findAllByMerchantAndContentLike(Merchant merchant, String content, Pageable pageable);
 
