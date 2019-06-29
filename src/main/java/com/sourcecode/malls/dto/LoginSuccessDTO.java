54da90e5571574fd76873c714d45473057bfb042
georgezeng <zzeng@bglcorp.com.au>
2019-06-29 23:11:49 +0800

    no message

--- src/main/java/com/sourcecode/malls/dto/LoginSuccessDTO.java
+++ src/main/java/com/sourcecode/malls/dto/LoginSuccessDTO.java
@@ -3,7 +3,16 @@ package com.sourcecode.malls.dto;
 import java.util.UUID;
 
 public class LoginSuccessDTO {
+	private Long userId;
 	private String token = UUID.randomUUID().toString();
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
 
 	public String getToken() {
 		return token;
