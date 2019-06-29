8c01de2ab3e36c94fbec79713a3b2231b140d9d8
georgezeng <zzeng@bglcorp.com.au>
2019-06-29 23:18:18 +0800

    no message

--- src/main/java/com/sourcecode/malls/web/security/strategy/LoginSuccessfulStrategy.java
+++ src/main/java/com/sourcecode/malls/web/security/strategy/LoginSuccessfulStrategy.java
@@ -13,17 +13,23 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sourcecode.malls.dto.LoginSuccessDTO;
 import com.sourcecode.malls.dto.base.ResultBean;
+import com.sourcecode.malls.properties.SessionAttributesProperties;
 
 @Component
 public class LoginSuccessfulStrategy implements RedirectStrategy {
 	@Autowired
 	private ObjectMapper mapper;
 
+	@Autowired
+	private SessionAttributesProperties sessionProperties;
+
 	@Override
 	public void sendRedirect(HttpServletRequest request, HttpServletResponse response, String url) throws IOException {
+		LoginSuccessDTO dto = new LoginSuccessDTO();
+		dto.setUserId((Long) request.getSession().getAttribute(sessionProperties.getUserId()));
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-		response.getWriter().write(mapper.writeValueAsString(new ResultBean<>(new LoginSuccessDTO())));
+		response.getWriter().write(mapper.writeValueAsString(new ResultBean<>(dto)));
 		response.getWriter().flush();
 		response.getWriter().close();
 	}
