fb1f6cbd80d092c242ea2a46d0753e6e1c08b892
georgezeng <zzeng@bglcorp.com.au>
2019-07-14 15:19:16 +0800

    no message

--- src/main/java/com/sourcecode/malls/domain/client/WechatToken.java
+++ src/main/java/com/sourcecode/malls/domain/client/WechatToken.java
@@ -2,6 +2,9 @@ package com.sourcecode.malls.domain.client;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import com.sourcecode.malls.domain.base.LongKeyEntity;
 
@@ -13,9 +16,16 @@ public class WechatToken extends LongKeyEntity {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@NotNull(message = "用户不能为空")
 	private Long userId;
+	@Size(max = 255, message = "openId长度不能大于255")
+	@NotBlank(message = "openId不能为空")
 	private String openId;
+	@Size(max = 255, message = "accessToken长度不能大于255")
+	@NotBlank(message = "accessToken不能为空")
 	private String accessToken;
+	@Size(max = 255, message = "refreshToken长度不能大于255")
+	@NotBlank(message = "refreshToken不能为空")
 	private String refreshToken;
 
 	public Long getUserId() {
