db5343f175f3b83b7be005f504a2e08e72839a99
georgezeng <zzeng@bglcorp.com.au>
2019-07-18 11:22:14 +0800

    no message

--- src/main/java/com/sourcecode/malls/domain/listener/CommonUpdateListener.java
+++ src/main/java/com/sourcecode/malls/domain/listener/CommonUpdateListener.java
@@ -6,12 +6,17 @@ import java.util.Date;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.sourcecode.malls.context.UserContext;
 import com.sourcecode.malls.domain.base.BaseEntity;
 import com.sourcecode.malls.domain.system.User;
 import com.sourcecode.malls.util.DateUtil;
 
 public class CommonUpdateListener {
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@PrePersist
 	public void PrePersist(Object object) {
 		BaseEntity entity = (BaseEntity) object;
@@ -21,6 +26,7 @@ public class CommonUpdateListener {
 		entity.setUpdateBy(currentUser.getUsername());
 
 		Date now = DateUtil.now();
+		logger.info(now.toString());
 		entity.setCreateTime(now);
 		entity.setUpdateTime(now);
 	}
