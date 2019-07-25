3695b2e6428266a38f4579f3466aec67698e8e9b
georgezeng <zzeng@bglcorp.com.au>
2019-07-25 18:14:44 +0800

    no message

--- src/main/java/com/sourcecode/malls/enums/OrderStatus.java
+++ src/main/java/com/sourcecode/malls/enums/OrderStatus.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 @JsonFormat(shape = Shape.OBJECT)
 public enum OrderStatus {
 	UnPay("待付款", "待付款"), Paid("待发货", "已付款"), Shipped("待收货", "已发货"), Canceled("已取消", "已取消"), Closed("已关闭", "已关闭"),
-	Finished("已完成", "已完成");
+	Finished("交易成功", "交易成功");
 
 	private String clientText;
 	private String merchantText;
