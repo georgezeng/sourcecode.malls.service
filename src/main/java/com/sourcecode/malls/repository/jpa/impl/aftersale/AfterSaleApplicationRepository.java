d6affde5cd0f5a20e497081a2eeaaec4fd67fd12
georgezeng <zzeng@bglcorp.com.au>
2019-07-25 18:08:15 +0800

    no message

--- src/main/java/com/sourcecode/malls/repository/jpa/impl/aftersale/AfterSaleApplicationRepository.java
+++ src/main/java/com/sourcecode/malls/repository/jpa/impl/aftersale/AfterSaleApplicationRepository.java
@@ -1,6 +1,7 @@
 package com.sourcecode.malls.repository.jpa.impl.aftersale;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,11 +10,14 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import com.sourcecode.malls.domain.aftersale.AfterSaleApplication;
 import com.sourcecode.malls.domain.client.Client;
 import com.sourcecode.malls.domain.order.Order;
+import com.sourcecode.malls.domain.order.SubOrder;
 import com.sourcecode.malls.enums.AfterSaleStatus;
 
 public interface AfterSaleApplicationRepository
 		extends JpaRepository<AfterSaleApplication, Long>, JpaSpecificationExecutor<AfterSaleApplication> {
 	List<AfterSaleApplication> findAllByOrderAndStatus(Order order, AfterSaleStatus status, Pageable pageable);
+
+	Optional<AfterSaleApplication> findBySubOrder(SubOrder order);
 
 	List<AfterSaleApplication> findAllByClientAndStatus(Client client, AfterSaleStatus status, Pageable pageable);
 }
