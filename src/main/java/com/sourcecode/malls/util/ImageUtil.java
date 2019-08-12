c596770e4d935e3c767b41238328eb4a74d76f38
georgezeng <zzeng@bglcorp.com.au>
2019-08-12 20:26:17 +0800

    no message

---
+++ src/main/java/com/sourcecode/malls/util/ImageUtil.java
@@ -0,0 +1,58 @@
+package com.sourcecode.malls.util;
+
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+
+public class ImageUtil {
+
+	/**
+	 * 文本居中
+	 * 
+	 * @param g
+	 * @param text
+	 * @param startX
+	 * @param startY
+	 * @param width
+	 * @param height
+	 * @param font
+	 */
+	public static void drawCenteredString(Graphics g, String text, int startX, int startY, int width, int height,
+			Font font) {
+		// Get the FontMetrics
+		FontMetrics metrics = g.getFontMetrics(font);
+		// Determine the X coordinate for the text
+		int x = startX + (width - metrics.stringWidth(text)) / 2;
+		// Determine the Y coordinate for the text (note we add the ascent, as in java
+		// 2d 0 is top of the screen)
+		int y = startY + ((height - metrics.getHeight()) / 2) + metrics.getAscent();
+		// Set the font
+		g.setFont(font);
+		// Draw the String
+		g.drawString(text, x, y);
+	}
+
+	/**
+	 * 旋转图片为指定角度
+	 * 
+	 * @param bufferedimage 目标图像
+	 * @param degree        旋转角度
+	 * @return
+	 */
+	public static BufferedImage rotateImage(final BufferedImage bufferedimage, final int degree) {
+		int w = bufferedimage.getWidth();
+		int h = bufferedimage.getHeight();
+		int type = bufferedimage.getColorModel().getTransparency();
+		BufferedImage img;
+		Graphics2D graphics2d;
+		(graphics2d = (img = new BufferedImage(w, h, type)).createGraphics())
+				.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		graphics2d.rotate(Math.toRadians(degree), w / 2, h / 2);
+		graphics2d.drawImage(bufferedimage, 0, 0, null);
+		graphics2d.dispose();
+		return img;
+	}
+}
