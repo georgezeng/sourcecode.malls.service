190af8e81f5c43e07d2e9eeeb318f3263a7a59b5
georgezeng <zzeng@bglcorp.com.au>
2019-08-18 11:10:34 +0800

    no message

--- src/main/java/com/sourcecode/malls/repository/jpa/impl/coupon/CashCouponSettingRepository.java
+++ src/main/java/com/sourcecode/malls/repository/jpa/impl/coupon/CashCouponSettingRepository.java
@@ -8,8 +8,10 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import com.sourcecode.malls.domain.coupon.cash.CashCouponSetting;
 import com.sourcecode.malls.domain.merchant.Merchant;
 import com.sourcecode.malls.enums.CashCouponEventType;
+import com.sourcecode.malls.enums.CouponSettingStatus;
 
 public interface CashCouponSettingRepository
 		extends JpaRepository<CashCouponSetting, Long>, JpaSpecificationExecutor<CashCouponSetting> {
-	List<CashCouponSetting> findAllByMerchantAndEventType(Merchant merchant, CashCouponEventType eventType);
+	List<CashCouponSetting> findAllByMerchantAndEventTypeAndStatusAndEnabled(Merchant merchant,
+			CashCouponEventType eventType, CouponSettingStatus status, boolean enabled);
 }
