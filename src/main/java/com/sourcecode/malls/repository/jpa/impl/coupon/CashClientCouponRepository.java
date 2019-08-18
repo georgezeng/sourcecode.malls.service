7bc0b0b8f4ddfa6d562aea844ac557ee0bc0a701
georgezeng <zzeng@bglcorp.com.au>
2019-08-18 10:30:18 +0800

    no message

--- src/main/java/com/sourcecode/malls/repository/jpa/impl/coupon/CashClientCouponRepository.java
+++ src/main/java/com/sourcecode/malls/repository/jpa/impl/coupon/CashClientCouponRepository.java
@@ -1,9 +1,15 @@
 package com.sourcecode.malls.repository.jpa.impl.coupon;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import com.sourcecode.malls.domain.client.Client;
 import com.sourcecode.malls.domain.coupon.cash.CashClientCoupon;
+import com.sourcecode.malls.domain.coupon.cash.CashCouponSetting;
 
-public interface CashClientCouponRepository extends JpaRepository<CashClientCoupon, Long>, JpaSpecificationExecutor<CashClientCoupon> {
+public interface CashClientCouponRepository
+		extends JpaRepository<CashClientCoupon, Long>, JpaSpecificationExecutor<CashClientCoupon> {
+	Optional<CashClientCoupon> findByClientAndSetting(Client client, CashCouponSetting setting);
 }
--- src/main/java/com/sourcecode/malls/repository/jpa/impl/coupon/CashCouponSettingRepository.java
+++ src/main/java/com/sourcecode/malls/repository/jpa/impl/coupon/CashCouponSettingRepository.java
@@ -1,9 +1,15 @@
 package com.sourcecode.malls.repository.jpa.impl.coupon;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.sourcecode.malls.domain.coupon.cash.CashCouponSetting;
+import com.sourcecode.malls.domain.merchant.Merchant;
+import com.sourcecode.malls.enums.CashCouponEventType;
 
-public interface CashCouponSettingRepository extends JpaRepository<CashCouponSetting, Long>, JpaSpecificationExecutor<CashCouponSetting> {
+public interface CashCouponSettingRepository
+		extends JpaRepository<CashCouponSetting, Long>, JpaSpecificationExecutor<CashCouponSetting> {
+	List<CashCouponSetting> findAllByMerchantAndEventType(Merchant merchant, CashCouponEventType eventType);
 }
