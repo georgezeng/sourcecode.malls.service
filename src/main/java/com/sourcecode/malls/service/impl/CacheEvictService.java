40fd0e7db87d92600d6ecb0fda7911eb5f3936f4
georgezeng <zzeng@bglcorp.com.au>
2019-08-25 18:07:11 +0800

    no message

--- src/main/java/com/sourcecode/malls/service/impl/CacheEvictService.java
+++ src/main/java/com/sourcecode/malls/service/impl/CacheEvictService.java
@@ -13,7 +13,7 @@ public class CacheEvictService {
 			@CacheEvict(cacheNames = "client_unuse_coupon_nums", key = "#clientId", condition = "#clientId != null"),
 			@CacheEvict(cacheNames = "client_coupon_nums", key = "#clientId + '-UnUse'", condition = "#clientId != null"),
 			@CacheEvict(cacheNames = "client_coupon_nums", key = "#clientId + '-Used'", condition = "#clientId != null"),
-			@CacheEvict(cacheNames = "client_coupon_nums", key = "#clientId + '-Out'", condition = "#clientId != null")})
+			@CacheEvict(cacheNames = "client_coupon_nums", key = "#clientId + '-Out'", condition = "#clientId != null") })
 	public void clearClientCoupons(Long clientId) {
 	}
 
@@ -21,20 +21,16 @@ public class CacheEvictService {
 	public void clearClientInvitePoster(Long clientId) {
 	}
 
-	@Caching(evict = {
-			@CacheEvict(cacheNames = "client_order_nums", key = "#clientId + '-UnPay'"),
+	@Caching(evict = { @CacheEvict(cacheNames = "client_order_nums", key = "#clientId + '-UnPay'"),
 			@CacheEvict(cacheNames = "client_order_nums", key = "#clientId + '-Paid'"),
-			@CacheEvict(cacheNames = "client_order_nums", key = "#clientId + '-Shipped'"),
-			@CacheEvict(cacheNames = "client_order_nums", key = "#clientId + '-Canceled'"),
-			@CacheEvict(cacheNames = "client_order_nums", key = "#clientId + '-Closed'"),
-			@CacheEvict(cacheNames = "client_order_nums", key = "#clientId + '-Finished'"), })
+			@CacheEvict(cacheNames = "client_order_nums", key = "#clientId + '-Shipped'") })
 	public void clearClientOrders(Long clientId) {
 	}
-	
+
 	@CacheEvict(cacheNames = "client_uncomment_nums", key = "#clientId")
 	public void clearClientUnCommentNums(Long clientId) {
 	}
-	
+
 	@CacheEvict(cacheNames = "client_aftersale_unfinished_nums", key = "#clientId")
 	public void clearClientAfterSaleUnFinishedtNums(Long clientId) {
 	}
