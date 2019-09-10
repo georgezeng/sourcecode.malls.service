5579fe3b20dfbe9eb3170c867d33fc7d31885c1f
georgezeng <zzeng@bglcorp.com.au>
2019-09-10 19:20:25 +0800

    no message

--- src/main/java/com/sourcecode/malls/dto/order/OrderDTO.java
+++ src/main/java/com/sourcecode/malls/dto/order/OrderDTO.java
@@ -43,6 +43,7 @@ public class OrderDTO extends SimpleQueryDTO {
 	private List<OrderCouponDTO> coupons;
 	private boolean comment;
 	private BigDecimal discount;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private Date refundTime;
 	private String clientLevelName;
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
