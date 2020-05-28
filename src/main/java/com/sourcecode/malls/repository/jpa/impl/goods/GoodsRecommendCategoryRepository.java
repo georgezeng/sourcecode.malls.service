18904281ae502d855d992b1e06c5ef60c5bdea1d
georgezeng <zzeng@bglcorp.com.au>
2020-05-28 15:03:10 +0800

    no message

--- src/main/java/com/sourcecode/malls/repository/jpa/impl/goods/GoodsRecommendCategoryRepository.java
+++ src/main/java/com/sourcecode/malls/repository/jpa/impl/goods/GoodsRecommendCategoryRepository.java
@@ -1,10 +1,15 @@
 package com.sourcecode.malls.repository.jpa.impl.goods;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import com.sourcecode.malls.domain.goods.GoodsCategory;
 import com.sourcecode.malls.domain.goods.GoodsRecommendCategory;
+import com.sourcecode.malls.domain.merchant.Merchant;
 
-public interface GoodsRecommendCategoryRepository extends JpaRepository<GoodsRecommendCategory, Long>, JpaSpecificationExecutor<GoodsRecommendCategory> {
-
+public interface GoodsRecommendCategoryRepository
+		extends JpaRepository<GoodsRecommendCategory, Long>, JpaSpecificationExecutor<GoodsRecommendCategory> {
+	List<GoodsRecommendCategory> findByMerchantAndCategory(Merchant merchant, GoodsCategory category);
 }
