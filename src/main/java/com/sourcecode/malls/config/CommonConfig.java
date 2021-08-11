627243fd97e3cb30a3e42e45d263feaa7397ecf1
GeorgeZeng <feleio@qq.com>
2021-08-11 17:32:28 +0800

    no message

--- src/main/java/com/sourcecode/malls/config/CommonConfig.java
+++ src/main/java/com/sourcecode/malls/config/CommonConfig.java
@@ -1,21 +1,19 @@
 package com.sourcecode.malls.config;
 
+import com.sourcecode.malls.properties.RedisSessionProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.web.client.RestTemplate;
-
-import com.sourcecode.malls.properties.RedisSessionProperties;
 
 @Configuration
 //@EnableCaching
@@ -38,13 +36,28 @@ public class CommonConfig {
 		return new RestTemplate();
 	}
 
+//	@Bean
+//	public LettuceConnectionFactory connectionFactory() {
+//		LettuceConnectionFactory factory = new LettuceConnectionFactory();
+//		factory.getStandaloneConfiguration().setHostName(redisProperties.getHost());
+//		factory.getStandaloneConfiguration().setPort(redisProperties.getPort());
+//		factory.getStandaloneConfiguration().setPassword(RedisPassword.of(redisProperties.getPassword()));
+//		return factory;
+//	}
+
 	@Bean
-	public LettuceConnectionFactory connectionFactory() {
-		LettuceConnectionFactory factory = new LettuceConnectionFactory();
-		factory.getStandaloneConfiguration().setHostName(redisProperties.getHost());
-		factory.getStandaloneConfiguration().setPort(redisProperties.getPort());
-		factory.getStandaloneConfiguration().setPassword(RedisPassword.of(redisProperties.getPassword()));
-		return factory;
+	public LettuceConnectionFactory redisConnectionFactory(
+			RedisProperties redisProperties) {
+		return new LettuceConnectionFactory(
+				redisProperties.getRedisHost(),
+				redisProperties.getRedisPort());
+	}
+
+	@Bean
+	public RedisTemplate<?, ?> redisTemplate(LettuceConnectionFactory connectionFactory) {
+		RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		return template;
 	}
 
 }
